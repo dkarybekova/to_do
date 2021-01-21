ca8c1aaf1805fc0ecf04373b660784d010dd7802
dkarybekova <turganakmoldo@gmail.com>
2021-01-21 10:41:30 +0600

    added model Book

--- main/models.py
+++ main/models.py
@@ -6,3 +6,12 @@ class ToDo(models.Model):
     is_favourite = models.BooleanField(default=False)
     is_done = models.BooleanField(default=False)
     
+class Book(models.Model):
+    title = models.CharField(max_length=50)
+    subtitle = models.CharField(max_length=50)
+    description = models.Textfield(max_length=500)
+    price = models.IntegerField(min_value=1)
+    genre = models.CharField(max_length=50)
+    author = models.CharField(max_length=50)
+    year = models.IntegerField(max_length=4)
+    date = models.DateField(auto_now_add=True)
