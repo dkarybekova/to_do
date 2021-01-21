dee185a74411bb2124fae7f96d8ac618190ae192
dkarybekova <turganakmoldo@gmail.com>
2021-01-21 10:25:05 +0600

    added todo_list

--- main/views.py
+++ main/views.py
@@ -1,7 +1,9 @@
 from django.shortcuts import render, HttpResponse
+from .models import ToDo
 
 def homepage(request):
-    return render(request, 'index.html')
+    todo_list = ToDo.objects.all()
+    return render(request, 'index.html', {'todo_list': todo_list})
 
 def add(request):
     return render(request, 'add.html')
